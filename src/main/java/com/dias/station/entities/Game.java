b26a5f7f5e15fc64304e01cd4405bd74898a7fb5
Italo Santos Dias <italosol200@hotmail.com>
2025-01-21 22:55:41 -0300

    criacao da tabela de entidade e de suas entidades

---
+++ src/main/java/com/dias/station/entities/Game.java
@@ -0,0 +1,132 @@
+package com.dias.station.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "jogos")
+public class Game {
+
+	
+	@Id  
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private Integer ano;
+	private String genero;
+	private String plataforma;
+	private Double nota;
+	private String urlImagem;
+	private String descricaoLonga;
+	private String descricaoCurta;
+	
+	public Game(Long id, String titulo, Integer ano, String genero, String plataforma, Double nota, String urlImagem,
+			String descricaoLonga, String descricaoCurta) {
+		
+		this.id = id;
+		this.titulo = titulo;
+		this.ano = ano;
+		this.genero = genero;
+		this.plataforma = plataforma;
+		this.nota = nota;
+		this.urlImagem = urlImagem;
+		this.descricaoLonga = descricaoLonga;
+		this.descricaoCurta = descricaoCurta;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public Double getNota() {
+		return nota;
+	}
+
+	public void setNota(Double nota) {
+		this.nota = nota;
+	}
+
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+
+	public String getDescricaoLonga() {
+		return descricaoLonga;
+	}
+
+	public void setDescricaoLonga(String descricaoLonga) {
+		this.descricaoLonga = descricaoLonga;
+	}
+
+	public String getDescricaoCurta() {
+		return descricaoCurta;
+	}
+
+	public void setDescricaoCurta(String descricaoCurta) {
+		this.descricaoCurta = descricaoCurta;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
